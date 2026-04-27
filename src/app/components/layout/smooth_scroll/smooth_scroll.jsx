a77ee6b4d005db23a7258c0c668183a6be927b44
TMINT DEV <gondetgendat@gmail.com>
2026-04-27 09:46:08 +0700

    fix lenis

--- src/app/components/layout/smooth_scroll/smooth_scroll.jsx
+++ src/app/components/layout/smooth_scroll/smooth_scroll.jsx
@@ -4,18 +4,13 @@ import Lenis from "lenis";
 
 export default function Smooth_scroll({ children }) {
   useEffect(() => {
-    const lenis = new Lenis({
-      lerp: 0.7,
-    });
+    const lenis = new Lenis();
 
     function raf(time) {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
-
     requestAnimationFrame(raf);
-
-    return () => lenis.destroy();
   }, []);
 
   return children;
