e0f7a2e0eb9e378bcb1aa2db015eeb4b300f43bf
TMINT DEV <gondetgendat@gmail.com>
2026-07-03 15:08:27 +0700

    refactor: migrate website data fetching to NestJS API

--- src/services/website.service.js
+++ src/services/website.service.js
@@ -1,34 +1,14 @@
-import { supabase } from "../lib/supabase";
-
 export async function getWebsiteData() {
-    const [sectionRes, lookbookItemsRes] = await Promise.all([
-        supabase.from("web_sections").select("*"),
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/website`,
+        {
+            cache: "no-store",
+        }
+    );
 
-        supabase
-            .from("lookbook_items")
-            .select("*")
-            .eq("is_published", true),
-    ]);
-
-    if (sectionRes.error) throw sectionRes.error;
-    if (lookbookItemsRes.error) throw lookbookItemsRes.error;
-
-    console.log(sectionRes.data);
-
-    const sectionMap = {};
-
-    sectionRes.data.forEach((section) => {
-        sectionMap[section.section_slug] = {
-            id: section.id,
-            ...(section.ui_payload || {}),
-        };
-    });
-
-    if (sectionMap.lookbook) {
-        sectionMap.lookbook.items = lookbookItemsRes.data;
+    if (!res.ok) {
+        throw new Error("Failed to Fetch Website Data");
     }
 
-    console.log(sectionMap)
-
-    return sectionMap;
+    return res.json();
 }
